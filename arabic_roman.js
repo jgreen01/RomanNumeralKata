481d2147cc3a992af5e94551da6bdaf419eabd7f
Jon Green <jgreen82@gatech.edu>
2015-07-06 12:17:11 -0400

    Feature 1 and 2 completed and tested.

--- arabic_roman.js
+++ arabic_roman.js
@@ -4,9 +4,9 @@ var AR = function() {
 
 /** Converts arabic numbers to roman numerals given an array of numerals.
   *
-  *  input: A number less that 4 times the value of the largest numeral.
-  *  numerals: An array of roman numerals arranged highest to lowest. First/largest numeral must be a power of ten
-  *    and array must have an odd number of elements.
+  *  input: A number less that 4 times the value of the largest numeral in the numerals array.
+  *  numerals: (optional) An array of roman numerals arranged highest to lowest. First/largest numeral
+  *    must be a power of ten and array must have an odd number of elements.
   */
 AR.prototype.arabicToRoman = function(input, numerals) {
 
@@ -14,22 +14,23 @@ AR.prototype.arabicToRoman = function(input, numerals) {
   if (typeof(numerals)==='undefined') numerals = this._romanNumerals.concat();
 
   var output = '',
-      base = Math.pow(10,Math.floor(numerals.length/2)), // base: The value of the largest numeral.
+      power = Math.pow(10,Math.floor(numerals.length/2)), // power: The value of the largest numeral.
       // optimized code to use the fewest number of calculations
-      baseBelow = base/10, fiveBelow = 5*baseBelow,
-      fourBelow = 4*baseBelow, nineBelow = 9*baseBelow,
-      numOverBase = input/base, numOverBaseRounded = Math.floor(numOverBase);
+      powerBelow = power/10, fiveBelow = 5*powerBelow,
+      fourBelow = 4*powerBelow, numOvrPwr = input/power;
 
   // if input has repeatable numerals ie I, X, C, or M
-  if(numOverBase >= 1 && numOverBaseRounded < 4) {
-    for( var i = 0; i < numOverBaseRounded; i++) output += numerals[0];
-    input %= base;
+  if(numOvrPwr >= 1 && numOvrPwr < 4) {
+    for( var i = 0, numOvrPwrRnd = Math.floor(numOvrPwr); i < numOvrPwrRnd; i++)
+      output += numerals[0];
+    input %= power;
   }
 
   // if input is a nine or four
   if(input%fiveBelow >= fourBelow) { // input%5 >= 4 is always true for input%10 >= 9
+    var nineBelow = 9*powerBelow;
     // if input is nine
-    if(input%base >= nineBelow) {
+    if(input%power >= nineBelow) {
       output += numerals[2] + numerals[0]; // ie IX
       input -= nineBelow;
     } else {
@@ -45,13 +46,20 @@ AR.prototype.arabicToRoman = function(input, numerals) {
   }
 
   if(input !== 0 && numerals.length !== 0){
-    numerals.shift(); numerals.shift(); // remove top two numeral from array
+    numerals.shift(); numerals.shift(); // remove the two largest numeral from array
     output += this.arabicToRoman(input,numerals);
   }
 
   return output;
 }
 
+
+/** Converts roman numerals to arabic numbers given an array of numerals.
+  *
+  *  input: A string of roman numerals less that 4 times the value of the largest numeral in the numerals array.
+  *  numerals: (optional) An array of roman numerals arranged highest to lowest. First/largest numeral
+  *    must be a power of ten and array must have an odd number of elements.
+  */
 AR.prototype.romanToArabic = function(input, numerals) {
 
   // if numerals aren't provided copy _romanNumerals
@@ -61,39 +69,36 @@ AR.prototype.romanToArabic = function(input, numerals) {
   if (typeof(input) === 'string') input = input.split('');
 
   var output = 0,
-      base = Math.pow(10,Math.floor(numerals.length/2)), // base: The value of the largest numeral.
-      // optimized code to use the fewest number of calculations
-      baseBelow = Math.floor(base/10), fiveBelow = 5*baseBelow,
-      fourBelow = 4*baseBelow, nineBelow = 9*baseBelow;
+      power = Math.pow(10,Math.floor(numerals.length/2)); // power: The value of the largest numeral.
 
   // repeatable numerals ie I, X, C, or M
   while(input[0] === numerals[0]){
-    output += base;
+    output += power;
     input.shift();
   }
 
   // if input is possibly a IV or IX
-  if(input[0] === numerals[2]){
+  if(input[0] === numerals[2]){ // e.g. input[0] == 'I' while the largest numeral (numerals[0]) is 'X'
     // if input is IX
-    if(input[1] === numerals[0]){
-      output += nineBelow;
+    if(input[1] === numerals[0]){ // the next numeral is the largest numeral
+      output += 9*Math.floor(power/10);
       input.shift(); input.shift();
     }
     // if input is IV
-    else if(input[1] === numerals[1]){
-      output += fourBelow;
+    else if(input[1] === numerals[1]){ // the next numeral is the second largest numeral
+      output += 4*Math.floor(power/10);
       input.shift(); input.shift();
     }
   }
 
   // if input is a V
   if(input[0] === numerals[1]){
-    output += fiveBelow;
+    output += 5*Math.floor(power/10);
     input.shift();
   }
 
   if(input.length !== 0 && numerals.length !== 0){
-    numerals.shift(); numerals.shift(); // remove top two numeral from array
+    numerals.shift(); numerals.shift(); // remove the two largest numeral from array
     output += this.romanToArabic(input,numerals);
   }
 
--- arabic_roman_test.spec.js
+++ arabic_roman_test.spec.js
@@ -1,221 +1,286 @@
 var AR = require('./arabic_roman.js');
 
 describe("Arabic numeral to Roman numeral", function() {
-  var a2r = new AR();
+  var ar = new AR();
 
-  it("One. Offical Test.", function() {
-    var result = a2r.arabicToRoman(1);
+  it("1 to I Required Test.", function() {
+    var result = ar.arabicToRoman(1);
     expect(result).toEqual('I');
   });
 
-  it("Two", function() {
-    var result = a2r.arabicToRoman(2);
+  it("2 to II.", function() {
+    var result = ar.arabicToRoman(2);
     expect(result).toEqual('II');
   });
 
-  it("Three. Offical Test.", function() {
-    var result = a2r.arabicToRoman(3);
+  it("3 to III. Required Test.", function() {
+    var result = ar.arabicToRoman(3);
     expect(result).toEqual('III');
   });
 
-  it("Four", function() {
-    var result = a2r.arabicToRoman(4);
+  it("4 to IV.", function() {
+    var result = ar.arabicToRoman(4);
     expect(result).toEqual('IV');
   });
 
-  it("Five", function() {
-    var result = a2r.arabicToRoman(5);
+  it("5 to V.", function() {
+    var result = ar.arabicToRoman(5);
     expect(result).toEqual('V');
   });
 
-  it("Six", function() {
-    var result = a2r.arabicToRoman(6);
+  it("6 to VI.", function() {
+    var result = ar.arabicToRoman(6);
     expect(result).toEqual('VI');
   });
 
-  it("Seven", function() {
-    var result = a2r.arabicToRoman(7);
+  it("7 to VII.", function() {
+    var result = ar.arabicToRoman(7);
     expect(result).toEqual('VII');
   });
 
-  it("Eight", function() {
-    var result = a2r.arabicToRoman(8);
+  it("8 to VIII.", function() {
+    var result = ar.arabicToRoman(8);
     expect(result).toEqual('VIII');
   });
 
-  it("Nine. Offical Test.", function() {
-    var result = a2r.arabicToRoman(9);
+  it("9 to IX. Required Test.", function() {
+    var result = ar.arabicToRoman(9);
     expect(result).toEqual('IX');
   });
 
-  it("Ten", function() {
-    var result = a2r.arabicToRoman(10);
+  it("10 to X.", function() {
+    var result = ar.arabicToRoman(10);
     expect(result).toEqual('X');
   });
 
-  it("Thirteen", function() {
-    var result = a2r.arabicToRoman(13);
+  it("13 to XIII.", function() {
+    var result = ar.arabicToRoman(13);
     expect(result).toEqual('XIII');
   });
 
-  it("Fourteen", function() {
-    var result = a2r.arabicToRoman(14);
+  it("14 to XIV.", function() {
+    var result = ar.arabicToRoman(14);
     expect(result).toEqual('XIV');
   });
 
-  it("Fifteen", function() {
-    var result = a2r.arabicToRoman(15);
+  it("15 to XV.", function() {
+    var result = ar.arabicToRoman(15);
     expect(result).toEqual('XV');
   });
 
-  it("Eighteen", function() {
-    var result = a2r.arabicToRoman(18);
+  it("18 to XVIII.", function() {
+    var result = ar.arabicToRoman(18);
     expect(result).toEqual('XVIII');
   });
 
-  it("Nineteen", function() {
-    var result = a2r.arabicToRoman(19);
+  it("19 to XIX.", function() {
+    var result = ar.arabicToRoman(19);
     expect(result).toEqual('XIX');
   });
 
-  it("Twenty", function() {
-    var result = a2r.arabicToRoman(20);
+  it("20 to XX.", function() {
+    var result = ar.arabicToRoman(20);
     expect(result).toEqual('XX');
   });
 
-  it("Thirty", function() {
-    var result = a2r.arabicToRoman(30);
+  it("30 to XXX.", function() {
+    var result = ar.arabicToRoman(30);
     expect(result).toEqual('XXX');
   });
 
-  it("Thirty eight", function() {
-    var result = a2r.arabicToRoman(38);
+  it("38 to XXXVIII.", function() {
+    var result = ar.arabicToRoman(38);
     expect(result).toEqual('XXXVIII');
   });
 
-  it("Thirty nine", function() {
-    var result = a2r.arabicToRoman(39);
+  it("39 to XXXIX.", function() {
+    var result = ar.arabicToRoman(39);
     expect(result).toEqual('XXXIX');
   });
 
-  it("Forty", function() {
-    var result = a2r.arabicToRoman(40);
+  it("40 to XL.", function() {
+    var result = ar.arabicToRoman(40);
     expect(result).toEqual('XL');
   });
 
-  it("Fifty", function() {
-    var result = a2r.arabicToRoman(50);
+  it("50 to L.", function() {
+    var result = ar.arabicToRoman(50);
     expect(result).toEqual('L');
   });
 
-  it("Sixty", function() {
-    var result = a2r.arabicToRoman(60);
+  it("60 to LX.", function() {
+    var result = ar.arabicToRoman(60);
     expect(result).toEqual('LX');
   });
 
-  it("Eighty", function() {
-    var result = a2r.arabicToRoman(80);
+  it("80 to LXXX.", function() {
+    var result = ar.arabicToRoman(80);
     expect(result).toEqual('LXXX');
   });
 
-  it("Ninety", function() {
-    var result = a2r.arabicToRoman(90);
+  it("90 to XC.", function() {
+    var result = ar.arabicToRoman(90);
     expect(result).toEqual('XC');
   });
 
-  it("101", function() {
-    var result = a2r.arabicToRoman(101);
+  it("101 to CI.", function() {
+    var result = ar.arabicToRoman(101);
     expect(result).toEqual('CI');
   });
 
-  it("212", function() {
-    var result = a2r.arabicToRoman(212);
+  it("212 to CCXII.", function() {
+    var result = ar.arabicToRoman(212);
     expect(result).toEqual('CCXII');
   });
 
-  it("353", function() {
-    var result = a2r.arabicToRoman(353);
+  it("353 to CCCLIII.", function() {
+    var result = ar.arabicToRoman(353);
     expect(result).toEqual('CCCLIII');
   });
 
-  it("404", function() {
-    var result = a2r.arabicToRoman(404);
+  it("404 to CDIV.", function() {
+    var result = ar.arabicToRoman(404);
     expect(result).toEqual('CDIV');
   });
 
-  it("555", function() {
-    var result = a2r.arabicToRoman(555);
+  it("555 to DLV.", function() {
+    var result = ar.arabicToRoman(555);
     expect(result).toEqual('DLV');
   });
 
-  it("846", function() {
-    var result = a2r.arabicToRoman(846);
+  it("846 to DCCCXLVI.", function() {
+    var result = ar.arabicToRoman(846);
     expect(result).toEqual('DCCCXLVI');
   });
 
-  it("927", function() {
-    var result = a2r.arabicToRoman(927);
+  it("927 to CMXXVII.", function() {
+    var result = ar.arabicToRoman(927);
     expect(result).toEqual('CMXXVII');
   });
 
-  it("1008", function() {
-    var result = a2r.arabicToRoman(1008);
+  it("1008 to MVIII.", function() {
+    var result = ar.arabicToRoman(1008);
     expect(result).toEqual('MVIII');
   });
 
-  it("1066. Offical Test.", function() {
-    var result = a2r.arabicToRoman(1066);
+  it("1066 to MLXVI. Required Test.", function() {
+    var result = ar.arabicToRoman(1066);
     expect(result).toEqual('MLXVI');
   });
 
-  it("1989. Offical Test.", function() {
-    var result = a2r.arabicToRoman(1989);
+  it("1989 to MCMLXXXIX. Required Test.", function() {
+    var result = ar.arabicToRoman(1989);
     expect(result).toEqual('MCMLXXXIX');
   });
 
-  it("1999", function() {
-    var result = a2r.arabicToRoman(1999);
+  it("1999 to MCMXCIX", function() {
+    var result = ar.arabicToRoman(1999);
     expect(result).toEqual('MCMXCIX');
   });
 
-  it("2420", function() {
-    var result = a2r.arabicToRoman(2420);
+  it("2420 to MMCDXX.", function() {
+    var result = ar.arabicToRoman(2420);
     expect(result).toEqual('MMCDXX');
   });
 
-  it("3888", function() {
-    var result = a2r.arabicToRoman(3888);
+  it("3888 to MMMDCCCLXXXVIII.", function() {
+    var result = ar.arabicToRoman(3888);
     expect(result).toEqual('MMMDCCCLXXXVIII');
   });
 
-  it("3999", function() {
-    var result = a2r.arabicToRoman(3999);
+  it("3999 to MMMCMXCIX.", function() {
+    var result = ar.arabicToRoman(3999);
     expect(result).toEqual('MMMCMXCIX');
   });
 
-  it("I to 1. Offical Test.", function() {
-    var result = a2r.romanToArabic('I');
+  it("I to 1. Required Test.", function() {
+    var result = ar.romanToArabic('I');
     expect(result).toEqual(1);
   });
 
-  it("III to 3. Offical Test.", function() {
-    var result = a2r.romanToArabic('III');
+  it("II to 2.", function() {
+    var result = ar.romanToArabic('II');
+    expect(result).toEqual(2);
+  });
+
+  it("III to 3. Required Test.", function() {
+    var result = ar.romanToArabic('III');
     expect(result).toEqual(3);
   });
 
-  it("IX to 9. Offical Test.", function() {
-    var result = a2r.romanToArabic('IX');
+  it("IV to 4.", function() {
+    var result = ar.romanToArabic('IV');
+    expect(result).toEqual(4);
+  });
+
+  it("V to 5.", function() {
+    var result = ar.romanToArabic('V');
+    expect(result).toEqual(5);
+  });
+
+  it("VI to 6.", function() {
+    var result = ar.romanToArabic('VI');
+    expect(result).toEqual(6);
+  });
+
+  it("VII to 7.", function() {
+    var result = ar.romanToArabic('VII');
+    expect(result).toEqual(7);
+  });
+
+  it("VIII to 8.", function() {
+    var result = ar.romanToArabic('VIII');
+    expect(result).toEqual(8);
+  });
+
+  it("IX to 9. Required Test.", function() {
+    var result = ar.romanToArabic('IX');
     expect(result).toEqual(9);
   });
 
-  it("MLXVI to 1066. Offical Test.", function() {
-    var result = a2r.romanToArabic('MLXVI');
+  it("X to 10.", function() {
+    var result = ar.romanToArabic('X');
+    expect(result).toEqual(10);
+  });
+
+  it("CDIV to 404.", function() {
+    var result = ar.romanToArabic('CDIV');
+    expect(result).toEqual(404);
+  });
+
+  it("DLV to 555.", function() {
+    var result = ar.romanToArabic('DLV');
+    expect(result).toEqual(555);
+  });
+
+  it("DCCCXLVI to 846.", function() {
+    var result = ar.romanToArabic('DCCCXLVI');
+    expect(result).toEqual(846);
+  });
+
+  it("MLXVI to 1066. Required Test.", function() {
+    var result = ar.romanToArabic('MLXVI');
     expect(result).toEqual(1066);
   });
 
-  it("MCMLXXXIX to 1989. Offical Test.", function() {
-    var result = a2r.romanToArabic('MCMLXXXIX');
+  it("MCMLXXXIX to 1989. Required Test.", function() {
+    var result = ar.romanToArabic('MCMLXXXIX');
     expect(result).toEqual(1989);
+  });
+
+  it("MMCDXX to 2420", function() {
+    var result = ar.romanToArabic('MMCDXX');
+    expect(result).toEqual(2420);
+  });
+
+  it("MMMDCCCLXXXVIII to 3888", function() {
+    var result = ar.romanToArabic('MMMDCCCLXXXVIII');
+    expect(result).toEqual(3888);
+  });
+
+  it("MMMCMXCIX to 3999", function() {
+    var result = ar.romanToArabic('MMMCMXCIX');
+    expect(result).toEqual(3999);
   });
 
 });
